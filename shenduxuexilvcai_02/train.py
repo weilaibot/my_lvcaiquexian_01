722bdaac0779a4efd70d82c181ce6734eaea0097
sanyauChenCoder <Cmyysd@163.com>
2025-04-06 17:49:17 +0800

    first commit

---
+++ shenduxuexilvcai_02/train.py
@@ -0,0 +1,23 @@
+import warnings, os
+
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/11/yolo11n.yaml')
+    model.train(data='dataset/data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=200,
+                batch=64,
+                close_mosaic=0,
+                workers=4,
+                # device='0,1',
+                optimizer='SGD',
+                # patience=0,
+                # resume=True,
+                # amp=False,
+                # fraction=0.2,
+                project='runs/train',
+                name='exp',
+                )
