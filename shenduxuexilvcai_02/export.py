c9c86515d5dcef0ef7d93f61a0c6017c814fd27e
weilaibot <Cmyysd@163.com>
2025-05-24 17:54:46 +0800

    Update export.py

--- shenduxuexilvcai_02/export.py
+++ shenduxuexilvcai_02/export.py
@@ -2,9 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
-# onnx onnxsim onnxruntime onnxruntime-gpu
 
-# 导出参数官方详解链接：https://docs.ultralytics.com/modes/export/#usage-examples
 
 if __name__ == '__main__':
     model = YOLO('yolov8n.pt')
